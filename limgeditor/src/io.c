cb34b47cb5b748f0023b71237fec2116d0569c94
mibi88 <mbcontact50@gmail.com>
2023-03-15 22:09:07 +0100

    Added some code

--- limgeditor/src/io.c
+++ limgeditor/src/io.c
@@ -1,4 +1,5 @@
 #include "io.h"
+
 void io_init(Io *io) {
      io->limg_data = NULL;
      io->x = 0;
@@ -21,35 +22,4 @@ void io_init(Io *io) {
      io->fcur = 0;
      io->state = 4;
      io->ask_changestate = 0;
-}
-
-void editor_input(Io *io) {
-    /* Move the cursor */
-    if(kbd_kdown(KUP) && io->y > 0) io->y--;
-    if(kbd_kdown(KDOWN) && io->y < io->limg.h-1) io->y++;
-    if(kbd_kdown(KLEFT) && io->x > 0) io->x--;
-    if(kbd_kdown(KRIGHT) && io->x < io->limg.w-1) io->x++;
-    /* Fix ix and iy */
-    while(io->x > io->ix+VWIDTH/io->scale-1) io->ix++;
-    while(io->x < io->ix) io->ix--;
-    while(io->y > io->iy+VHEIGHT/io->scale-1) io->iy++;
-    while(io->y < io->iy) io->iy--;
-    if(io->x >= io->limg.w) io->x = io->limg.w - 1;
-    if(io->y >= io->limg.h) io->y = io->limg.h - 1;
-    /* Other actions */
-    if(kbd_kdown(KC)){
-        kbd_setrepeat(KLEFT, 0);
-        kbd_setrepeat(KRIGHT, 0);
-        kbd_setrepeat(KX, 0);
-        io->state = 2;
-    }
-    if(kbd_kdown(KX)) limg_setpixel(io->x, io->y, io->color, &io->limg);
-    if(kbd_kdown(KV)){
-        kbd_setrepeat(KUP, 0);
-        kbd_setrepeat(KDOWN, 0);
-        kbd_setrepeat(KLEFT, 1);
-        kbd_setrepeat(KRIGHT, 1);
-        kbd_setrepeat(KX, 0);
-        io->state = 1;
-    }
 }
--- limgeditor/src/main.c
+++ limgeditor/src/main.c
@@ -45,213 +45,224 @@ int main(void) {
     uInit(396, 224, "Limg image editor", 4, 20);
     while(!uAskexit()){
         uClear((uint32_t)0xC8FC90);
-        if(state == 0){
-            editor_input(&io);
-        }else if(state == 1){
-            if(kbd_kdown(KX)){
-                ask_changestate = 1;
-            }else if(ask_changestate && !uKeydown(KX)){
-                kbd_setrepeat(KUP, moverepeat);
-                kbd_setrepeat(KDOWN, moverepeat);
-                kbd_setrepeat(KLEFT, moverepeat);
-                kbd_setrepeat(KRIGHT, moverepeat);
-                kbd_setrepeat(KX, 1);
-                ask_changestate = 0;
-                state = 0;
-            }
-            if(kbd_kdown(KUP)){
-                colorcur--;
-                if(colorcur < 0) colorcur = 2;
-            }
-            if(kbd_kdown(KDOWN)){
-                colorcur++;
-                if(colorcur > 2) colorcur = 0;
-            }
-            getrgb565(color, rgb);
-            if(kbd_kdown(KLEFT) && rgb[colorcur] > 0) rgb[colorcur]--;
-            if(kbd_kdown(KRIGHT) && (colorcur == 1 ? rgb[colorcur] < 63 : rgb[colorcur] < 31)) rgb[colorcur]++;
-            color = makergb565(rgb);
-        }else if(state == 2){
-            if(kbd_kdown(KLEFT) && menucur > 0) menucur--;
-            if(kbd_kdown(KRIGHT) && menucur < MENU_SIZE-1) menucur++;
-            if(kbd_kdown(KX)){
-                if(menucur == 0){
-                    scale++;
-                    if(scale > 8) scale = 1;
-                }else if(menucur == 1){
-                    ask_changestate = 2;
-                }else if(menucur == 2){
-                    kbd_setrepeat(KUP, 0);
-                    kbd_setrepeat(KDOWN, 0);
-                    kbd_setrepeat(KLEFT, 1);
-                    kbd_setrepeat(KRIGHT, 1);
-                    kbd_setrepeat(KX, 0);
+        switch(io.state){
+            case 0:
+                editor_input(&io);
+                break;
+            case 1:
+                if(kbd_kdown(KX)){
+                    ask_changestate = 1;
+                }else if(ask_changestate && !uKeydown(KX)){
+                    kbd_setrepeat(KUP, moverepeat);
+                    kbd_setrepeat(KDOWN, moverepeat);
+                    kbd_setrepeat(KLEFT, moverepeat);
+                    kbd_setrepeat(KRIGHT, moverepeat);
+                    kbd_setrepeat(KX, 1);
                     ask_changestate = 0;
-                    state = 1;
-                }else if(menucur == 3){
-                    color = limg_getpixel(x, y, &limg);
-                }else if(menucur == 4){
-                    initfilechooser(".");
-                    state = 6;
-                }else if(menucur == 5){
+                    state = 0;
+                }
+                if(kbd_kdown(KUP)){
+                    colorcur--;
+                    if(colorcur < 0) colorcur = 2;
+                }
+                if(kbd_kdown(KDOWN)){
+                    colorcur++;
+                    if(colorcur > 2) colorcur = 0;
+                }
+                getrgb565(color, rgb);
+                if(kbd_kdown(KLEFT) && rgb[colorcur] > 0) rgb[colorcur]--;
+                if(kbd_kdown(KRIGHT) && (colorcur == 1 ? rgb[colorcur] < 63 : rgb[colorcur] < 31)) rgb[colorcur]++;
+                color = makergb565(rgb);
+                break;
+            case 2:
+                if(kbd_kdown(KLEFT) && menucur > 0) menucur--;
+                if(kbd_kdown(KRIGHT) && menucur < MENU_SIZE-1) menucur++;
+                if(kbd_kdown(KX)){
+                    if(menucur == 0){
+                        scale++;
+                        if(scale > 8) scale = 1;
+                    }else if(menucur == 1){
+                        ask_changestate = 2;
+                    }else if(menucur == 2){
+                        kbd_setrepeat(KUP, 0);
+                        kbd_setrepeat(KDOWN, 0);
+                        kbd_setrepeat(KLEFT, 1);
+                        kbd_setrepeat(KRIGHT, 1);
+                        kbd_setrepeat(KX, 0);
+                        ask_changestate = 0;
+                        state = 1;
+                    }else if(menucur == 3){
+                        color = limg_getpixel(x, y, &limg);
+                    }else if(menucur == 4){
+                        initfilechooser(".");
+                        state = 6;
+                    }else if(menucur == 5){
+                        state = 4;
+                    }
+                }else if(ask_changestate == 2 && !uKeydown(KX)){
+                    moverepeat = !moverepeat;
+                    kbd_setrepeat(KUP, moverepeat);
+                    kbd_setrepeat(KDOWN, moverepeat);
+                    kbd_setrepeat(KLEFT, moverepeat);
+                    kbd_setrepeat(KRIGHT, moverepeat);
+                    kbd_setrepeat(KX, 1);
+                    ask_changestate = 0;
+                    state = 0;
+                }
+                if(kbd_kdown(KC)){
+                    ask_changestate = 1;
+                }else if(ask_changestate == 1 && !uKeydown(KC)){
+                    kbd_setrepeat(KLEFT, moverepeat);
+                    kbd_setrepeat(KRIGHT, moverepeat);
+                    kbd_setrepeat(KX, 1);
+                    ask_changestate = 0;
+                    state = 0;
+                }
+                break;
+            case 3:
+                kbd_setrepeat(KUP, 0);
+                kbd_setrepeat(KDOWN, 0);
+                kbd_setrepeat(KX, 0);
+                dirsize = getdirsize();
+                if(dirsize > 0){
+                    if(kbd_kdown(KUP) && dircur > 0) dircur--;
+                    if(kbd_kdown(KDOWN) && dircur < dirsize-1) dircur++;
+                    if(dircur > dirsize-1){
+                        dircur = dirsize-1;
+                    }
+                    while(dircur > dirview+F_AMOUNT-1) dirview++;
+                    while(dircur < dirview) dirview--;
+                }
+                if(kbd_kdown(KC)){
+                    ask_changestate = 1;
+                }else if(!kbd_kdown(KC) && ask_changestate){
+                    ask_changestate = 0;
                     state = 4;
                 }
-            }else if(ask_changestate == 2 && !uKeydown(KX)){
-                moverepeat = !moverepeat;
-                kbd_setrepeat(KUP, moverepeat);
-                kbd_setrepeat(KDOWN, moverepeat);
-                kbd_setrepeat(KLEFT, moverepeat);
-                kbd_setrepeat(KRIGHT, moverepeat);
-                kbd_setrepeat(KX, 1);
-                ask_changestate = 0;
-                state = 0;
-            }
-            if(kbd_kdown(KC)){
-                ask_changestate = 1;
-            }else if(ask_changestate == 1 && !uKeydown(KC)){
-                kbd_setrepeat(KLEFT, moverepeat);
-                kbd_setrepeat(KRIGHT, moverepeat);
-                kbd_setrepeat(KX, 1);
-                ask_changestate = 0;
-                state = 0;
-            }
-        }else if(state == 3){
-            kbd_setrepeat(KUP, 0);
-            kbd_setrepeat(KDOWN, 0);
-            kbd_setrepeat(KX, 0);
-            dirsize = getdirsize();
-            if(dirsize > 0){
-                if(kbd_kdown(KUP) && dircur > 0) dircur--;
-                if(kbd_kdown(KDOWN) && dircur < dirsize-1) dircur++;
-                if(dircur > dirsize-1){
-                    dircur = dirsize-1;
+                break;
+            case 4:
+                kbd_setrepeat(KUP, 0);
+                kbd_setrepeat(KDOWN, 0);
+                kbd_setrepeat(KX, 0);
+                if(kbd_kdown(KUP) && mainmenucur) mainmenucur = 0;
+                if(kbd_kdown(KDOWN) && !mainmenucur) mainmenucur = 1;
+                if(kbd_kdown(KX)){
+                    ask_changestate = 1;
+                }else if(!kbd_kdown(KX) && ask_changestate == 1){
+                    ask_changestate = 0;
+                    memset(wtext, '\0', 6);
+                    memset(htext, '\0', 6);
+                    wcur = 0;
+                    hcur = 0;
+                    ncur = 0;
+                    if(mainmenucur) initfilechooser(".");
+                    state = mainmenucur ? 3 : 5;
                 }
-                while(dircur > dirview+F_AMOUNT-1) dirview++;
-                while(dircur < dirview) dirview--;
-            }
-            if(kbd_kdown(KC)){
-                ask_changestate = 1;
-            }else if(!kbd_kdown(KC) && ask_changestate){
-                ask_changestate = 0;
-                state = 4;
-            }
-        }else if(state == 4){
-            kbd_setrepeat(KUP, 0);
-            kbd_setrepeat(KDOWN, 0);
-            kbd_setrepeat(KX, 0);
-            if(kbd_kdown(KUP) && mainmenucur) mainmenucur = 0;
-            if(kbd_kdown(KDOWN) && !mainmenucur) mainmenucur = 1;
-            if(kbd_kdown(KX)){
-                ask_changestate = 1;
-            }else if(!kbd_kdown(KX) && ask_changestate == 1){
-                ask_changestate = 0;
-                memset(wtext, '\0', 6);
-                memset(htext, '\0', 6);
-                wcur = 0;
-                hcur = 0;
-                ncur = 0;
-                if(mainmenucur) initfilechooser(".");
-                state = mainmenucur ? 3 : 5;
-            }
-            if(kbd_kdown(KV)){
-                ask_changestate = 2;
-            }else if(!kbd_kdown(KV) && ask_changestate == 2){
-                kbd_setrepeat(KUP, moverepeat);
-                kbd_setrepeat(KDOWN, moverepeat);
-                kbd_setrepeat(KLEFT, moverepeat);
-                kbd_setrepeat(KRIGHT, moverepeat);
-                kbd_setrepeat(KX, 1);
-                ask_changestate = 0;
-                state = 0;
-            }
-        }else if(state == 5){
-            kbd_setrepeat(KUP, 0);
-            kbd_setrepeat(KDOWN, 0);
-            kbd_setrepeat(KLEFT, 0);
-            kbd_setrepeat(KRIGHT, 0);
-            kbd_setrepeat(KX, 0);
-            if(kbd_kdown(KUP) && ncur) ncur = 0;
-            if(kbd_kdown(KDOWN) && !ncur) ncur = 1;
-            if(!ncur) intinput(wtext, &wcur, 5);
-            if(ncur) intinput(htext, &hcur, 5);
-            if(kbd_kdown(KX)){
-                ask_changestate = 1;
-            }else if(!kbd_kdown(KX) && ask_changestate){
-                w = atoi(wtext);
-                h = atoi(htext);
-                if(w < 1) w = 1;
-                if(h < 1) h = 1;
-                limg_free(&limg);
-                out = limg_create(w, h, 0x0000, &limg);
-                kbd_setrepeat(KUP, moverepeat);
-                kbd_setrepeat(KDOWN, moverepeat);
-                kbd_setrepeat(KLEFT, moverepeat);
-                kbd_setrepeat(KRIGHT, moverepeat);
-                kbd_setrepeat(KX, 1);
-                ask_changestate = 0;
-                state = out < 0 ? -1 : 0;
-            }
-        }else if(state == 6){
-            kbd_setrepeat(KUP, 0);
-            kbd_setrepeat(KDOWN, 0);
-            kbd_setrepeat(KX, 0);
-            dirsize = getdirsize();
-            if(dirsize > 0){
-                if(kbd_kdown(KUP) && dircur > 0) dircur--;
-                if(kbd_kdown(KDOWN) && dircur < dirsize-1) dircur++;
-                if(dircur > dirsize-1){
-                    dircur = dirsize-1;
+                if(kbd_kdown(KV)){
+                    ask_changestate = 2;
+                }else if(!kbd_kdown(KV) && ask_changestate == 2){
+                    kbd_setrepeat(KUP, moverepeat);
+                    kbd_setrepeat(KDOWN, moverepeat);
+                    kbd_setrepeat(KLEFT, moverepeat);
+                    kbd_setrepeat(KRIGHT, moverepeat);
+                    kbd_setrepeat(KX, 1);
+                    ask_changestate = 0;
+                    state = 0;
                 }
-                while(dircur > dirview+F_AMOUNT-1) dirview++;
-                while(dircur < dirview) dirview--;
-            }
-            if(kbd_kdown(KC)){
-                ask_changestate = 2;
-            }else if(!kbd_kdown(KC) && ask_changestate == 2){
-                ask_changestate = 0;
-                state = 4;
-            }
-        }else if(state == 7){
-            if(kbd_kdown(KX)){
-                ask_changestate = 1;
-            }else if(!kbd_kdown(KX) && ask_changestate){
-                kbd_setrepeat(KUP, moverepeat);
-                kbd_setrepeat(KDOWN, moverepeat);
-                kbd_setrepeat(KLEFT, moverepeat);
-                kbd_setrepeat(KRIGHT, moverepeat);
-                kbd_setrepeat(KX, 1);
-                strcat(_file, ftext);
-                strcat(_file, ".limg");
-                /* Save */
-                out = limg_encode(&limg_data, &limg);
-                if(out >= 0){
-                    fp = fopen(_file, "wb");
-                    if(fp != NULL){
-                        fwrite((char*)limg_data, 1, out, fp);
-                        fclose(fp);
-                    }else state = -3;
-                }else state = -4;
-                ask_changestate = 0;
-                state = 0;
-            }
-            if(!ask_changestate) fnameinput(ftext, &fcur, 8);
-        }else if(state < 0){
-            kbd_setrepeat(KX, 0);
-            if(kbd_kdown(KX)){
-                ask_changestate = 1;
-            }else if(!kbd_kdown(KX) && ask_changestate){
-                kbd_setrepeat(KUP, moverepeat);
-                kbd_setrepeat(KDOWN, moverepeat);
-                kbd_setrepeat(KLEFT, moverepeat);
-                kbd_setrepeat(KRIGHT, moverepeat);
-                kbd_setrepeat(KX, 1);
-                kbd_setrepeat(KC, 0);
-                kbd_setrepeat(KV, 0);
-                state = 0;
-                ask_changestate = 0;
-            }
-        }else{
-            state = 0;
+                break;
+            case 5:
+                kbd_setrepeat(KUP, 0);
+                kbd_setrepeat(KDOWN, 0);
+                kbd_setrepeat(KLEFT, 0);
+                kbd_setrepeat(KRIGHT, 0);
+                kbd_setrepeat(KX, 0);
+                if(kbd_kdown(KUP) && ncur) ncur = 0;
+                if(kbd_kdown(KDOWN) && !ncur) ncur = 1;
+                if(!ncur) intinput(wtext, &wcur, 5);
+                if(ncur) intinput(htext, &hcur, 5);
+                if(kbd_kdown(KX)){
+                    ask_changestate = 1;
+                }else if(!kbd_kdown(KX) && ask_changestate){
+                    w = atoi(wtext);
+                    h = atoi(htext);
+                    if(w < 1) w = 1;
+                    if(h < 1) h = 1;
+                    limg_free(&limg);
+                    out = limg_create(w, h, 0x0000, &limg);
+                    kbd_setrepeat(KUP, moverepeat);
+                    kbd_setrepeat(KDOWN, moverepeat);
+                    kbd_setrepeat(KLEFT, moverepeat);
+                    kbd_setrepeat(KRIGHT, moverepeat);
+                    kbd_setrepeat(KX, 1);
+                    ask_changestate = 0;
+                    state = out < 0 ? -1 : 0;
+                }
+                break;
+            case 6:
+                kbd_setrepeat(KUP, 0);
+                kbd_setrepeat(KDOWN, 0);
+                kbd_setrepeat(KX, 0);
+                dirsize = getdirsize();
+                if(dirsize > 0){
+                    if(kbd_kdown(KUP) && dircur > 0) dircur--;
+                    if(kbd_kdown(KDOWN) && dircur < dirsize-1) dircur++;
+                    if(dircur > dirsize-1){
+                        dircur = dirsize-1;
+                    }
+                    while(dircur > dirview+F_AMOUNT-1) dirview++;
+                    while(dircur < dirview) dirview--;
+                }
+                if(kbd_kdown(KC)){
+                    ask_changestate = 2;
+                }else if(!kbd_kdown(KC) && ask_changestate == 2){
+                    ask_changestate = 0;
+                    state = 4;
+                }
+                break;
+            case 7:
+                if(kbd_kdown(KX)){
+                    ask_changestate = 1;
+                }else if(!kbd_kdown(KX) && ask_changestate){
+                    kbd_setrepeat(KUP, moverepeat);
+                    kbd_setrepeat(KDOWN, moverepeat);
+                    kbd_setrepeat(KLEFT, moverepeat);
+                    kbd_setrepeat(KRIGHT, moverepeat);
+                    kbd_setrepeat(KX, 1);
+                    strcat(_file, ftext);
+                    strcat(_file, ".limg");
+                    /* Save */
+                    out = limg_encode(&limg_data, &limg);
+                    if(out >= 0){
+                        fp = fopen(_file, "wb");
+                        if(fp != NULL){
+                            fwrite((char*)limg_data, 1, out, fp);
+                            fclose(fp);
+                        }else state = -3;
+                    }else state = -4;
+                    ask_changestate = 0;
+                    state = 0;
+                }
+                if(!ask_changestate) fnameinput(ftext, &fcur, 8);
+                break;
+            default:
+                if(state < 0){
+                    kbd_setrepeat(KX, 0);
+                    if(kbd_kdown(KX)){
+                        ask_changestate = 1;
+                    }else if(!kbd_kdown(KX) && ask_changestate){
+                        kbd_setrepeat(KUP, moverepeat);
+                        kbd_setrepeat(KDOWN, moverepeat);
+                        kbd_setrepeat(KLEFT, moverepeat);
+                        kbd_setrepeat(KRIGHT, moverepeat);
+                        kbd_setrepeat(KX, 1);
+                        kbd_setrepeat(KC, 0);
+                        kbd_setrepeat(KV, 0);
+                        state = 0;
+                        ask_changestate = 0;
+                    }
+                }else{
+                    state = 0;
+                }
         }
         draw_limg(&limg, 32, 0, VWIDTH, VHEIGHT, ix, iy, scale, x, y);
         vline(31, 0, VHEIGHT, 0x588C28);
